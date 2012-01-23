0e2301865b5ee732c7cd3eca3f953baa05b47d75
Cristian Cutocheras <ccutocheras@kobo.com>
2012-01-23 21:53:39 +0000

    Former-commit-id: f45c6905c1f8b35902339718d7e4cba79c415986 Former-commit-id: 28fe39028e6bba4431b7c30774651da4517e65c7

--- src/3rdparty/webkit/WebKit/qt/Api/qwebframe.h
+++ src/3rdparty/webkit/WebKit/qt/Api/qwebframe.h
@@ -201,6 +201,8 @@ public:
 
     QWebSecurityOrigin securityOrigin() const;
 
+    bool selectionIntersectsElement(const QString &nodeName, const QString &className, QString &id);
+
 public Q_SLOTS:
     QVariant evaluateJavaScript(const QString& scriptSource);
 #ifndef QT_NO_PRINTER
